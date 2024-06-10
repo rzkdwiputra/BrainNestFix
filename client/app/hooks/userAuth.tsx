9aaaf7efa031271c7df5b9a4ac66f1c1b7241968
rzkdwiputra <rizkydwiputra6666@gmail.com>
2024-06-10 17:22:08 +0700

    terakhir dah

--- client/app/hooks/userAuth.tsx
+++ client/app/hooks/userAuth.tsx
@@ -1,11 +1,7 @@
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
-export default function userAuth() {
+export default function useUserAuth() {
   const { user } = useSelector((state: any) => state.auth);
 
-  if (user) {
-    return true;
-  } else {
-    return false;
-  }
+  return Boolean(user);
 }
