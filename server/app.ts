dba1a97a3ac243048e98e0188203d14472c296ba
rzkdwiputra <rizkydwiputra6666@gmail.com>
2024-06-10 19:08:13 +0700

    Cors di tambah

--- server/app.ts
+++ server/app.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 //cor => cross origin resource sharing
 app.use(
   cors({
-    origin: ["https://brainnest.vercel.app"],
+    origin: ["http://localhost:3000", "https://brainnest.vercel.app"],
     credentials: true,
   })
 );
